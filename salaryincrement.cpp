249d6f2f190bec88fe0097da08cf1342b44fc3bf
Rajeev Enugu <rrajeevenugu@gmail.com>
2025-08-27 20:14:10 +0530

    Add files via upload

---
+++ salaryincrement.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void ref(float &profit) {
+    profit = profit + (profit *0.20);
+}
+int main() {
+    int id, projects, publications, new_proj_in_pipeline;
+    float profit, salary;
+
+    cout << "Enter Employee ID: ";
+    cin >> id;
+    cout << "Enter projects done: ";
+    cin >> projects;
+    cout << "Enter publications done: ";
+    cin >> publications;
+    cout << "Enter profit (Rs): ";
+    cin >> profit;
+    cout << "Enter salary (Rs): ";
+    cin >> salary;
+    cout << "Enter new projects in pipeline: ";
+    cin >> new_proj_in_pipeline;
+
+    if (projects > 0 && publications > 0 && profit >= 100000 && new_proj_in_pipeline > 0) {
+        
+        cout<<"ID: "<<id<<endl;
+        ref(salary);
+        cout<<"After increment salary: "<<salary<<endl;
+    }
+    else{
+        cout<<"Salary will not increase for: ID"<<id<<endl;
+    }
+    return 0;
+}
